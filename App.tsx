8183dceff43dcef5d9bb71f4efab85f938d6732e
Bijaya Adhikari <bjadhs@gmail.com>
2025-06-07 20:52:06 +1000

    Setup Tailwindcss Native

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import './global.css';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='flex-1 items-center justify-center bg-accent dark:bg-blue-500'>
+      <Text className='text-primary dark:text-white text-2xl font-bold border-2 border-primary p-4 rounded-md'>
+        Hi Bijaya
+      </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.tsx", "./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF",
+        accent: "#FACC15",
+      },
+    },
+  },
+  plugins: [],
+}
+
